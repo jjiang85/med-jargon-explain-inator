e4e73b296787398971dc2b9dd9201c982c2e25b4
Emma McKibbin <echm@patas.ling.washington.edu>
2024-05-23 13:16:36 -0700

    added def retrieval endpoint

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ def read_root():
 @app.get("/explain/{term}")
 def explain(term: str):
     return controller.explain(term)
+
+@app.get("/define/{term}")
+def define(term: str):
+    term_file = "data/term_to_cui.json"
+    def_file = "data/cui_to_def.json"
+    return controller.define(term, term_file, def_file)
--- model/controller.py
+++ model/controller.py
@@ -1,4 +1,8 @@
 import nltk
+from model.term_def_retrieval.retrieval import get_definition
 
 def explain(term: str) -> dict:
     return {"term": term}
+
+def define(term: str, term_file: str, def_file: str) -> dict:
+    return get_definition(term, term_file, def_file)
